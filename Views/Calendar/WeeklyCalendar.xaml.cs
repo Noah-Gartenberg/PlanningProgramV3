6a780e028a728c573afc1e56d2ddbb97742f7e89
Noah Gartenberg <156126383+Noah-Gartenberg@users.noreply.github.com>
2025-09-05 16:16:08 -0400

    Created Helper Method class, and moved GetFirstDayOfWeek method into it.

--- Views/Calendar/WeeklyCalendar.xaml.cs
+++ Views/Calendar/WeeklyCalendar.xaml.cs
@@ -478,42 +478,5 @@ namespace PlanningProgramV3.Views.Calendar
             }
             return (date - firstMonthMonday).Days / 7 + 1;
         }
-
-        /**
-         * Helper method to get the first day of the week in which the date that is the parameter is within.
-         * @param: date - DateTime - the date which the user wants to search for the first day of the week that it is part of
-         */
-        public static DateTime GetFirstDayOfWeekDate(DateTime date)
-        {
-            DayOfWeek day = date.DayOfWeek;
-            int daysFromStartOfWeek = 0;
-            //figure out how many days from start of the week the current date is. 
-            switch (day)
-            {
-                case DayOfWeek.Monday:
-                    daysFromStartOfWeek = 0;
-                    break;
-                case DayOfWeek.Tuesday:
-                    daysFromStartOfWeek = 1;
-                    break;
-                case DayOfWeek.Wednesday:
-                    daysFromStartOfWeek = 2;
-                    break;
-                case DayOfWeek.Thursday:
-                    daysFromStartOfWeek = 3;
-                    break;
-                case DayOfWeek.Friday:
-                    daysFromStartOfWeek = 4;
-                    break;
-                case DayOfWeek.Saturday:
-                    daysFromStartOfWeek = 5;
-                    break;
-                case DayOfWeek.Sunday:
-                    daysFromStartOfWeek = 6;
-                    break;
-            }
-
-            return new DateTime(date.Year, date.Month, date.AddDays(-1 * daysFromStartOfWeek).Day);
-        }
     }
 }
