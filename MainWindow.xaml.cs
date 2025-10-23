6d107f563475169cd5579c7545762e09d31bade4
Noah Gartenberg <156126383+Noah-Gartenberg@users.noreply.github.com>
2025-10-23 12:09:01 -0400

    Put Plan Displayer Control into Main Window, rather than as a user control. Removed user control from project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,40 +1,21 @@
-﻿using System.Text;
-using System.Windows;
-using PlanningProgramV3.Views.PlanControls;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using PlanningProgramV3.Converters;
 using PlanningProgramV3.ViewModels;
-using PlanningProgramV3.Views;
-using PlanningProgramV3.Views.Calendar;
 using PlanningProgramV3.ViewModels.Calendar;
 using PlanningProgramV3.ViewModels.ItemViewModels;
+using PlanningProgramV3.Views.Calendar;
 using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PlanningProgramV3
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
+    ///     This is specifically for the "main window" part of the main window, rather than the plan displayer.
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
-    {
-
-        public static readonly DependencyProperty SelectedObjectProperty = DependencyProperty.Register(
-            "SelectedObject", typeof(FrameworkElement), typeof(ListView));
-
-        public FrameworkElement SelectedObject
-        {
-            get => (FrameworkElement)GetValue(SelectedObjectProperty);
-            set => SetValue(SelectedObjectProperty, value);
-        }
-
-        
-
+    {   
         public ICalendarDisplay CalendarInView;
         public MainWindow()
         {
@@ -127,5 +108,128 @@ namespace PlanningProgramV3
             e.CanExecute = (DataContext as MainWindowViewModel).SaveCurrentPlan.CanExecute(null);
         }
         #endregion
+    }
+
+    /// <summary>
+    /// Interaction logic/drag and drop for plan displayer in main window
+    ///     tbh, I dont' know why I put the plan displayer in a user control in the first place. This is going to be a bit easier
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public static readonly DependencyProperty SelectedObjectProperty = DependencyProperty.Register(
+            "SelectedObject", typeof(FrameworkElement), typeof(ListView));
+
+        public FrameworkElement SelectedObject
+        {
+            get => (FrameworkElement)GetValue(SelectedObjectProperty);
+            set => SetValue(SelectedObjectProperty, value);
+        }
+        private void AddTaskItem_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as MainWindowViewModel).AddTaskToCurrentPlan.Execute(null);
+        }
+
+        private void PlannerDisplayer_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                //how ensure that the sender is the list view? 
+                if (sender is ListView && (PlannerDisplayer.SelectedItem is TaskViewModel viewModel) && e.LeftButton == MouseButtonState.Pressed)
+                {
+
+                    DataObject data = new DataObject(DataFormats.Serializable, PlannerDisplayer.SelectedItem);
+
+                    DragDrop.DoDragDrop(SelectedObject, data, DragDropEffects.Move);
+                    if (SelectedObject != null)
+                    {
+                        SelectedObject.IsHitTestVisible = false;
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void PlannerDisplayer_DragOver(object sender, DragEventArgs e)
+        {
+            //For right now, will just not do selection logic if the selected item is not the highest level parent
+            if ((PlannerDisplayer.SelectedItem is TaskViewModel viewModel) && viewModel.Parent == null)
+            {
+                Point dropPosition = e.GetPosition(PlannerDisplayer);
+
+                Canvas.SetLeft(SelectedObject, dropPosition.X);
+                Canvas.SetTop(SelectedObject, dropPosition.Y);
+
+            }
+        }
+
+        private void PlannerDisplayer_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                object data = e.Data.GetData(DataFormats.Serializable);
+
+                Point dropPosition = e.GetPosition(PlannerDisplayer);
+                Canvas.SetLeft(SelectedObject, dropPosition.X);
+                Canvas.SetTop(SelectedObject, dropPosition.Y);
+                //CurrentPlanner.SetTopTaskPosition(SelectedObject);
+
+
+                //set the position of the object
+                //I know this isn't technically MVVM - I mean I don't think it's a violation per se, but yeah. 
+                //couldn't figure out how to do this from the planner
+                if (SelectedObject.DataContext is TaskViewModel task)
+                {
+                    task.X = Convert.ToDouble(PixelToCoordinate.ConvertToCoordinate(dropPosition.X, typeof(int), 1500, null));
+                    task.Y = Convert.ToDouble(PixelToCoordinate.ConvertToCoordinate(dropPosition.Y, typeof(int), 1500, null));
+                }
+                SelectedObject.IsHitTestVisible = true;
+                PlannerDisplayer.SelectedIndex = -1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+
+        private void PlannerDisplayer_DragLeave(object sender, DragEventArgs e)
+        {
+            if (e.OriginalSource == PlannerDisplayer)
+            {
+                object data = e.Data.GetData(DataFormats.Serializable);
+
+
+                //commented out for right now
+                //if (data is UIElement element)
+                //{
+                //    CurrentPlanner.DeleteItem(data);
+                //}
+            }
+        }
+
+        private void PlannerDisplayer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Shout out people in comments explaining how I can fix this so that selected object doesn't throw an error
+            //https://stackoverflow.com/questions/610343/wpf-listbox-getting-uielement-instead-of-of-selecteditem
+            //in theory at least if I can get it working
+
+            //for right now, only worrying about one item
+            if (SelectedObject != null)
+            {
+                SelectedObject.IsHitTestVisible = true;
+            }
+            if (PlannerDisplayer.SelectedIndex != -1)
+            {
+
+                SelectedObject = PlannerDisplayer.ItemContainerGenerator.ContainerFromItem(PlannerDisplayer.SelectedItem) as FrameworkElement;
+            }
+            else
+                SelectedObject = null;
+        }
     }
 }
